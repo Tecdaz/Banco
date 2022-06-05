1c5ae6065e20ad9ff398ebd81b255751f8a7ea8c
Santiago Arias <santi99oca@gmail.com>
2022-06-05 13:36:47 -0300

    add select_options function

--- services/console.py
+++ services/console.py
@@ -5,3 +5,21 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
+
+def select_options(message, options) -> str:
+        '''
+        Despliega un menu de opciones que viene dado por la lista/tupla options
+        Verifica que la opcion este en el rango de opciones disponibles
+        Retorna la opcion escogida
+        '''
+        clear()
+        print(message, end='\n')
+
+        for index, item in enumerate(options):
+            print(f'{index+1}. {item}')
+
+        option = int(input("Seleccione una opcion: "))
+        while option < 1 or option > len(options):
+            option = int(input("Seleccione una opcion valida: "))
+
+        return options[option-1]
