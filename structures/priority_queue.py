2bb150b0118b03b8930a14ce149e6e9a80367297
Santiago Arias <santi99oca@gmail.com>
2022-06-05 13:37:38 -0300

    Extends queue class adding conditions

---
+++ structures/priority_queue.py
@@ -0,0 +1,12 @@
+from services.console import select_options
+from structures.queue import Queue
+from services.bank import is_client, operations
+
+class Priority_queue(Queue):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conditions = []
+
+    def set_conditions(self):
+        self.conditions.append(select_options("El ususario debe ser cliente?", is_client))
+        self.conditions.append(select_options("Seleccione el tipo de tramite", operations))
