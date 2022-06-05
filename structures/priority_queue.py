f36efd830edf1984230c2868ef308c581682cdb2
Santiago Arias <santi99oca@gmail.com>
2022-06-05 13:42:35 -0300

    bool value for "is_client"

--- structures/priority_queue.py
+++ structures/priority_queue.py
@@ -8,5 +8,7 @@ class Priority_queue(Queue):
         self.conditions = []
 
     def set_conditions(self):
-        self.conditions.append(bool((select_options("El ususario debe ser cliente?", is_client))))
+        self.conditions.append(bool(
+            select_options("El ususario debe ser cliente?", is_client)
+            ))
         self.conditions.append(select_options("Seleccione el tipo de tramite", operations))
