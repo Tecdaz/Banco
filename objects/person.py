f73f6b095c31afa64a1f37c54a9cfa842c8eb2cd
Santiago Arias <santi99oca@gmail.com>
2022-06-05 13:36:21 -0300

    export select_options function to reuse it

--- objects/person.py
+++ objects/person.py
@@ -1,5 +1,4 @@
-from turtle import st
-from services.console import clear
+from services.console import clear, select_options
 from services.bank import operations, destinataries, is_client
 
 
@@ -13,24 +12,6 @@ class Person():
         self.is_client: bool = None
 
     # Private
-
-    def __select_options(self, message, options) -> str:
-        '''
-        Despliega un menu de opciones que viene dado por la lista/tupla options
-        Verifica que la opcion este en el rango de opciones disponibles
-        Retorna la opcion escogida
-        '''
-        clear()
-        print(message, end='\n')
-
-        for index, item in enumerate(options):
-            print(f'{index+1}. {item}')
-
-        option = int(input("Seleccione una opcion: "))
-        while option < 1 or option > len(options):
-            option = int(input("Seleccione una opcion valida: "))
-
-        return options[option-1]
 
     def __input_data(self, message) -> str:
         '''
@@ -60,8 +41,8 @@ Monto: {self.amount}
         '''Solicita datos para la persona'''
         self.name = self.__input_data("Ingrese el nombre")
         self.age = int(self.__input_data('Ingrese la edad'))
-        self.operation = self.__select_options('Ingrese su operacion', operations)
-        self.destinatary = self.__select_options('Ingrese su destinatario', destinataries)
+        self.operation = self.select_options('Ingrese su operacion', operations)
+        self.destinatary = self.select_options('Ingrese su destinatario', destinataries)
         self.amount = int(self.__input_data('Ingrese el monto'))
-        self.is_client = bool(self.__select_options('Es cliente?', is_client))
+        self.is_client = bool(self.select_options('Es cliente?', is_client))
 
