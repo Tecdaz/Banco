2c39050ef85c90743325d21908c04a0380bc018f
Santiago Arias <santi99oca@gmail.com>
2022-06-05 15:33:22 -0300

    dict structure for conditions

--- structures/priority_queue.py
+++ structures/priority_queue.py
@@ -5,10 +5,10 @@ from services.bank import is_client, operations
 class Priority_queue(Queue):
     def __init__(self) -> None:
         super().__init__()
-        self.conditions = []
+        self.conditions = {'is_client': None, 'tramit': None}
 
     def set_conditions(self):
-        self.conditions.append(bool(
+        self.conditions["is_client"] = bool(
             select_options("El ususario debe ser cliente?", is_client)
-            ))
-        self.conditions.append(select_options("Seleccione el tipo de tramite", operations))
+            )
+        self.conditions["tramit"] = select_options("Seleccione el tipo de tramite", operations)
